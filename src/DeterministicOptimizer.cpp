5f95af0509364913aed50972c98090534d52b5b4
MixTeam145 <mixteam145@gmail.com>
2023-12-12 20:57:17 +0300

    Small fix

--- src/DeterministicOptimizer.cpp
+++ src/DeterministicOptimizer.cpp
@@ -12,6 +12,8 @@ DeterministicOptimizer::~DeterministicOptimizer()
 
 double DeterministicOptimizer::max_direction_magnitude() const
 {
+	if (newton_direction.norm() < 1e-16)
+		return 0;
 	double magnitude = ((newton_direction[0] >= 0 ? domain.max_point[0] : domain.min_point[0]) - current_point[0]) / newton_direction[0];
 	for (int i = 1; i < newton_direction.size(); ++i)
 	{
@@ -25,7 +27,7 @@ double DeterministicOptimizer::max_direction_magnitude() const
 double DeterministicOptimizer::one_dim_optimization(double left_border, double right_border, double eps) const
 {
 	double l = left_border, r = right_border;
-	for (int i = 0; r - l > eps; ++i)
+	while (r - l > eps)
 	{
 		double tl = (2 * l + r) / 3, tr = (l + 2 * r) / 3;
 		if (f->eval(current_point + tl * newton_direction) < f->eval(current_point + tr * newton_direction))
